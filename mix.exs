5c7391148182076302768cd9d607ecbcee50f14b
JarredOlson <jolson@pillartechnology.com>
2019-03-21 11:28:49 -0400

    SMRT Bumping Version
    co-authored-by: Jake Prem <jakeprem@gmail.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.Registry.MixProject do
   def project do
     [
       app: :smart_city_registry,
-      version: "2.2.0",
+      version: "2.3.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
