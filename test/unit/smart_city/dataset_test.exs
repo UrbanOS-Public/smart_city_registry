fd2c204c3356202cf6d0b6951bb24db5cd42f104
Richard Jones <rajones@hntb.com>
2019-04-05 14:07:22 -0500

    SMRT-1094 Made it so that _metadata is optional in the schema
    co-authored-by: Scott Millard <smillard@hntb.com>

--- test/unit/smart_city/dataset_test.exs
+++ test/unit/smart_city/dataset_test.exs
@@ -93,7 +93,11 @@ defmodule SmartCity.DatasetTest do
     test "creates a private dataset by default", %{message: map} do
       %{"technical" => tech} = map
       technical = Technical.new(tech)
-      assert technical.private == true
+
+    test "can create a new dataset without _metadata in the schema", %{message: map, json: json} do
+      map_no_meta = Map.delete(map, "_metadata")
+
+      assert {:ok, _} = Dataset.new(map_no_meta)
     end
   end
 
