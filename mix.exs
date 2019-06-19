678e1e082244325a2fb269a3ed9a8575a8891251
ManApart <akucera@pillartechnology.com>
2019-06-19 15:36:49 -0400

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.Registry.MixProject do
   def project do
     [
       app: :smart_city_registry,
-      version: "3.0.0",
+      version: "3.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
