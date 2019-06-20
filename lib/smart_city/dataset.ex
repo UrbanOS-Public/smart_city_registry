72dbf7ace1578459d2e500fbe67f1adc46e751ce
ManApart <akucera@pillartechnology.com>
2019-06-20 10:18:57 -0400

    add a space

--- lib/smart_city/dataset.ex
+++ lib/smart_city/dataset.ex
@@ -41,7 +41,7 @@ defmodule SmartCity.Dataset do
         }
       ],
       "sourceUrl": "",
-      "protocol": "",       //List of protocols to use. Defaults to nil. Can be [http1, http2]
+      "protocol": "",       // List of protocols to use. Defaults to nil. Can be [http1, http2]
       "authUrl": "",
       "sourceFormat": "",
       "sourceType": "",     // remote|stream|batch
