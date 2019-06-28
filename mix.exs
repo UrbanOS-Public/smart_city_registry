2ddb21d6420ac216724573e3ac2e8e4725433a37
Scott Millard <smillard@hntb.com>
2019-06-28 09:41:34 -0400

    project_36 Updating deps
    co-authored-by: Richard Jones <rjones@hntb.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SmartCity.Registry.MixProject do
   def project do
     [
       app: :smart_city_registry,
-      version: "3.2.1",
+      version: "3.2.2",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
